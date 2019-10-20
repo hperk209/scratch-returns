f6287ba56b2e1f2d3d4044233db33f69c0d13454
Larry Parsons <lp.larry.parsons@gmail.com>
2019-10-20 15:57:19 -0700

    Adds
    Char - Height Speed.
    Bounds - Water

--- Source/newScratch/newScratchCharacter.cpp
+++ Source/newScratch/newScratchCharacter.cpp
@@ -20,7 +20,7 @@ DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
 AnewScratchCharacter::AnewScratchCharacter()
 {
 	// Set size for collision capsule
-	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
+	GetCapsuleComponent()->InitCapsuleSize(155.f, 196.0f);
 
 	// set our turn rates for input
 	BaseTurnRate = 45.f;
@@ -38,7 +38,7 @@ AnewScratchCharacter::AnewScratchCharacter()
 	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
 	Mesh1P->bCastDynamicShadow = false;
 	Mesh1P->CastShadow = false;
-	Mesh1P->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
+	//Mesh1P->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
 	Mesh1P->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
 
 	// Create a gun mesh component
